403f55cedf6b0a781b053b52cbed632889e0279f
mohanprasad16 <mohanprasad16101998@gmail.com>
2021-08-10 14:13:34 +0530

    Add a static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   get 'static_page/home'
   get 'static_page/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
